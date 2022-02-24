38454ad83e0c34041b51fb86347298e995fd93dd
Shannkar022 <shankarweb22@gmail.com>
2022-02-24 10:24:14 +0530

    Adding React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Mail from "./Mail";
+import EmailList from "./EmailList";
 
 function App() {
   return (
-    <div className="app">
-      <Header/>
-     <Sidebar/>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<EmailList />} />
+            <Route path="mail" element={<Mail />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,18 +2,17 @@ import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
 
-import { Button, IconButton} from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
-import InboxIcon from '@material-ui/icons/Inbox';
-import StarIcon from '@material-ui/icons/Star';
-import LabelImportantIcon from '@material-ui/icons/LabelImportant';
-import NearMeIcon from '@material-ui/icons/NearMe';
-import NoteIcon from '@material-ui/icons/Note';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import PersonIcon from '@material-ui/icons/Person';
-import DuoIcon from '@material-ui/icons/Duo';
-import PhoneIcon from '@material-ui/icons/Phone';
-
+import InboxIcon from "@material-ui/icons/Inbox";
+import StarIcon from "@material-ui/icons/Star";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 function Sidebar() {
   return (
@@ -23,28 +22,33 @@ function Sidebar() {
         className="sidebar__compose"
       >
         Compose
-          </Button>
-          
-          <SidebarOption Icon={InboxIcon} title="Inbox" number={54} selected={true}/>
-          <SidebarOption Icon={StarIcon} title="Starred" number={54} />
-          <SidebarOption Icon={LabelImportantIcon} title="Important" number={54} />
-          <SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
-          <SidebarOption Icon={NoteIcon} title="Drafts" number={54} />
-          <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+      </Button>
 
-          <div className="sidebar__footer">
-              <div className="sidebar__footerIcons">
-                  <IconButton>
-                      <PersonIcon/>
-                  </IconButton>
-                  <IconButton>
-                      <DuoIcon/>
-                  </IconButton>
-                  <IconButton>
-                      <PhoneIcon/>
-                    </IconButton>
-              </div>
-          </div>
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={54} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={54} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" number={54} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
